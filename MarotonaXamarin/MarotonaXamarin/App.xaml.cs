405ad8744cfe80ec42d34d357eeb66458b435b18
Nelson Campos <ncamposfh@gmail.com>
2019-04-09 15:17:59 -0300

    Adicionada dependencias e navegacao

---
+++ MarotonaXamarin/MarotonaXamarin/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace MarotonaXamarin
+{
+    public partial class App
+    {
+        public App() : this(null) { }
+        public App(IPlatformInitializer platformInitializer) : base(platformInitializer)
+        {
+        }
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry conteinerRegistry)
+        {
+            conteinerRegistry.RegisterForNavigation<NavigationPage>();
+            conteinerRegistry.RegisterForNavigation<Views.MainPage>();
+        }
+
+        //protected override void OnStart()
+        //{
+        //    // Handle when your app starts
+        //}
+
+        //protected override void OnSleep()
+        //{
+        //    // Handle when your app sleeps
+        //}
+
+        //protected override void OnResume()
+        //{
+        //    // Handle when your app resumes
+        //}
+    }
+}
---
+++ MarotonaXamarin/MarotonaXamarin/ViewsModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Prism.Navigation;
+
+namespace MarotonaXamarin.ViewsModels
+{
+    class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+
+        public void OnNavigatedForm(INavigationParameters parameters)
+        {
+
+        }
+    }
+}
